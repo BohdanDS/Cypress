120558da74e80f332b65b93d1df8be40e9b69905
BohdanDS <bohdanpeliutkevich@gmail.com>
2022-07-13 16:13:40 +0300

    ref

--- cypress/e2e/webdriver-uni/handle-radio-buttons.cy.js
+++ cypress/e2e/webdriver-uni/handle-radio-buttons.cy.js
@@ -1,21 +1,17 @@
 /// <reference types='Cypress' />
 
 describe("Handle radioButtons", () => {
-  it("Handle radioButtons", () => {
+  before(() => {
     cy.visit("https://webdriveruniversity.com");
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
       .click();
-
+  });
+  it("Handle radioButtons", () => {
     cy.get("#radio-buttons").find("[type='radio']").last().check();
     cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
   });
   it("Handle state of radioButtons", () => {
-    cy.visit("https://webdriveruniversity.com");
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click();
-
     //   cy.get("#radio-buttons-selected-disabled").
     cy.get("[value = 'lettuce']").should("not.be.checked");
     cy.get("[value = 'cabbage']").should("be.disabled");
---
+++ cypress/e2e/webdriver-uni/hooks.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="Cypress" />
+describe("Hooks", () => {
+  before(() => {
+    cy.log("runs once before all tests in the block");
+  });
+
+  beforeEach(() => {
+    cy.log(" runs before each test in the block");
+  });
+
+  afterEach(() => {
+    cy.log("runs after each test in the block");
+  });
+
+  after(() => {
+    cy.log("runs once after all tests in the block");
+  });
+  it("Test Example 1", () => {
+    cy.log("Example 1");
+  });
+  it("Test Example 2", () => {
+    cy.log("Example 2");
+  });
+});
